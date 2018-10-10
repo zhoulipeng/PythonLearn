97f1c2cbe6daee552f5f13cd685dd5070c1c7c66
zhoulipeng <zhoulpg@gmail.com>
2018-10-10 14:45:27 +0800

    gevent example

---
+++ gevent/gevent_wsgi.py
@@ -0,0 +1,41 @@
+from gevent import monkey; monkey.patch_all()
+from gevent.pywsgi import *
+
+def app(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ['<html><head><title>AB Test</title></head><body><h1>Hello</h1></body></html>']
+
+print('Start gevent 9999...')
+WSGIServer(('', 9999), app).serve_forever()
+"""
+$ ab -n 10000 -c 1000 -k http://localhost:9999/
+...
+Concurrency Level:      1000
+Time taken for tests:   4.514 seconds
+Complete requests:      10000
+Failed requests:        0
+Write errors:           0
+Total transferred:      1900000 bytes
+HTML transferred:       750000 bytes
+Requests per second:    2215.36 [#/sec] (mean)
+Time per request:       451.393 [ms] (mean)
+Time per request:       0.451 [ms] (mean, across all concurrent requests)
+Transfer rate:          411.05 [Kbytes/sec] received
+Connection Times (ms)
+              min  mean[+/-sd] median   max
+Connect:        0  105 350.3      9    3029
+Processing:    71  165  81.3    145    1747
+Waiting:       67  161  81.2    142    1744
+Total:         88  270 383.4    152    3395
+
+Percentage of the requests served within a certain time (ms)
+  50%    152
+  66%    165
+  75%    176
+  80%    205
+  90%    353
+  95%   1158
+  98%   1407
+  99%   1880
+ 100%   3395 (longest request)
+ """
